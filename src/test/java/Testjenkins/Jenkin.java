35ccad43c5c495b92a061239e20bad2c3fdbe848
raghuvarma987 <raghu.chennu44@gmail.com>
2020-07-05 13:03:36 +0530

    commiting new changes

--- src/test/java/Testjenkins/Jenkin.java
+++ src/test/java/Testjenkins/Jenkin.java
@@ -3,30 +3,33 @@ package Testjenkins;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Jenkin {
 	 WebDriver driver;
-	@BeforeTest
-	public void launchchrome() {
+	@BeforeMethod
+	public void launchchrome() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver",
 				"R:\\Testing\\Java\\chromedriver 71\\chromedriver.exe");
 	    driver = new ChromeDriver();
-	    driver.get("https:/www.google.in");
+	    driver.get("https://www.google.co.in/");
+	    Thread.sleep(6000);
 	    driver.manage().window().maximize();
 	}
 	@Test
 	public void ganatest(){
-		driver.findElement(By.xpath("//*[@id='q']")).sendKeys("gaana");
+		driver.findElement(By.xpath("//*[@name='q']")).sendKeys("gaana");
 	}
 	@Test
 	public void instatest(){
-		driver.findElement(By.xpath("//*[@id='q']")).sendKeys("insta");
+		driver.findElement(By.xpath("//*[@name='q']")).sendKeys("insta");
 	}
 	
-	@AfterTest
+	@AfterMethod
 	public void closechrome() {
 		driver.close();
 	}
