d209e9ca68f2d9169f34744ac1eeeb33c07d9b5f
raghuvarma987 <raghu.chennu44@gmail.com>
2020-07-05 12:12:55 +0530

    commiting file

--- src/test/java/Testjenkins/Jenkin.java
+++ src/test/java/Testjenkins/Jenkin.java
@@ -1,5 +1,6 @@
 package Testjenkins;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
@@ -7,23 +8,27 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Jenkin {
-	
+	 WebDriver driver;
 	@BeforeTest
-	public void jen() {
-		System.out.println("Genkins");
+	public void launchchrome() {
+		System.setProperty("webdriver.chrome.driver",
+				"R:\\Testing\\Java\\chromedriver 71\\chromedriver.exe");
+	    driver = new ChromeDriver();
+	    driver.get("https:/www.google.in");
+	    driver.manage().window().maximize();
 	}
 	@Test
-	public void launchchrome() {
-	System.setProperty("webdriver.chrome.driver",
-			"R:\\Testing\\Java\\chromedriver 71\\chromedriver.exe");
-    WebDriver driver = new ChromeDriver();
-    driver.get("https:/www.google.in");
-    driver.manage().window().maximize();
-    driver.close();
+	public void ganatest(){
+		driver.findElement(By.xpath("//*[@id='q']")).sendKeys("gaana");
 	}
+	@Test
+	public void instatest(){
+		driver.findElement(By.xpath("//*[@id='q']")).sendKeys("insta");
+	}
+	
 	@AfterTest
-	public void tess() {
-		System.out.println("Genkins2");
+	public void closechrome() {
+		driver.close();
 	}
 
 }
